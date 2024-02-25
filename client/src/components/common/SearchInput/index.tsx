83846304a3e9116c734e194fd779e0a68fa150aa
Merhan Mustafov <m.o.mustafov@gmail.com>
2024-02-25 19:35:28 +0200

    feat: Added search country functionality

--- client/src/components/common/SearchInput/index.tsx
+++ client/src/components/common/SearchInput/index.tsx
@@ -1,10 +1,24 @@
+import React from 'react';
 import { MdSearch } from 'react-icons/md';
 
-const SearchInput = () => {
+type SearchInputProps = {
+  searchInput: string;
+  setSearchInput: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const SearchInput: React.FC<SearchInputProps> = (props) => {
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.setSearchInput(e.target.value);
+  };
   return (
-    <div className="focus-within:shadow-searchInput flex flex-nowrap items-center border-[1px] border-slate-900 focus-within:border-transparent ">
+    <div className="flex flex-nowrap items-center border-[1px] border-slate-900 focus-within:border-transparent focus-within:shadow-searchInput ">
       <MdSearch className=" w-[20%] cursor-pointer pl-1 text-2xl" />
-      <input className="w-full p-1 outline-none" type="text" />
+      <input
+        className="w-full p-1 outline-none"
+        type="text"
+        value={props.searchInput}
+        onChange={handleSearchInputChange}
+      />
     </div>
   );
 };
